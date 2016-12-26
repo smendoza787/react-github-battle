b0c54cfe2661658073a802cdd7f6f5fce2c3e1e7
Sergio Mendoza <smendoza787@gmail.com>
2016-12-25 21:54:53 -0800

    finishing up the React components

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,9 +1,41 @@
 var React = require('react');
+var PropTypes = React.PropTypes;
+var styles = require('../styles');
+
+function puke(object) {
+  return <pre>{JSON.stringify(object, null, ' ')}</pre>
+}
 
 function ConfirmBattle (props) {
   return props.isLoading === true
     ? <p> LOADING! </p>
-    : <p> CONFIRM BATTLE! </p>
+    : <div className='jumbotron col-sm-12 text-center' style={styles.transparentBg}>
+        <h1>Confirm Players</h1>
+        <div className='col-sm-8 col-sm-offset-2'>
+          <div className='col-sm-6'>
+            <p className='lead'>Player 1</p>
+            PLAYER 1 INFO
+          </div>
+          <div className='col-sm-6'>
+            <p className='lead'>Player 2</p>
+            PLAYER 2 INFO
+          </div>
+        </div>
+        <div className='col-sm-8 col-sm-offset-2'>
+          <div className='col-sm-12' style={styles.space}>
+            INITIATE BATTLE BUTTON
+          </div>
+          <div className='col-sm-12' style={styles.space}>
+            LINK TO /PLAYERONE
+          </div>
+        </div>
+      </div>
+}
+
+ConfirmBattle.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+  onInitiateBattle: PropTypes.func.isRequired,
+  playersInfo: PropTypes.array.isRequired,
 }
 
 module.exports = ConfirmBattle;
--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var ConfirmBattle = require('../components/ConfirmBattle');
+var githubHelpers = require('../utils/githubHelpers')
 
 var ConfirmBattleContainer = React.createClass({
   contextTypes: {
@@ -13,12 +14,27 @@ var ConfirmBattleContainer = React.createClass({
   },
   componentDidMount: function () {
     var query = this.props.location.query;
-    // fetch info from github then update state
+    githubHelpers.getPlayersInfo([query.playerOne, query.playerTwo])
+      .then(function (players) {
+        this.setState({
+          isLoading: false,
+          playersInfo: [players[0], players[1]]
+        })
+      }.bind(this))
+  },
+  handleInitiateBattle: function () {
+    this.context.router.push({
+      pathname: '/results',
+      state: {
+        playerInfo: this.state.playersInfo
+      }
+    })
   },
   render: function() {
     return (
       <ConfirmBattle
         isLoading={this.state.isLoading}
+        onInitiateBattle={this.handleInitiateBattle}
         playersInfo={this.state.playersInfo} />
     );
   }
