e095c66786949d5c617c26e5702a60cd8a22eb77
Sergio Mendoza <smendoza787@gmail.com>
2016-12-30 23:11:39 -0800

    added more github helpers to pull data and determine winner

--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -8,6 +8,24 @@ function getUserInfo (username) {
   return axios.get('https://api.github.com/users/' + username + param)
 }
 
+function getRepos (username) {
+  // fetch username repos
+}
+
+function getTotalStars (stars) {
+  // calculate all the stars that the user has
+}
+
+function getPlayersData (player) {
+  // get repos
+  // get totalStars
+  // return object with that data
+}
+
+function calculateScores (players) {
+  // return an array, after doing some fancy algorithms to determine a winner
+}
+
 var helpers = {
   getPlayersInfo: function (players) {
     return axios.all(players.map(function(username) {
@@ -19,6 +37,9 @@ var helpers = {
     }).catch(function (err) {
       console.warn('Error in getPlayersInfo', err)
     })
+  },
+  battle: function (players) {
+
   }
 };
 
