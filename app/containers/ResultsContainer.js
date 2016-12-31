cd05df72b604f913cac4d1a4e191f9374874db10
Sergio Mendoza <smendoza787@gmail.com>
2016-12-30 22:11:20 -0800

    updated results container

--- app/containers/ResultsContainer.js
+++ app/containers/ResultsContainer.js
@@ -2,10 +2,28 @@ var React = require('react');
 var Results = require('../components/Results')
 
 var ResultsContainer = React.createClass({
-
+  getInitialState: function() {
+    return {
+      isLoading: true,
+      scores: []
+    }
+  },
+  componentDidMount: function() {
+    console.log(this.props.location.state.playersInfo);
+    githubHelpers.battle(this.props.location.state.playersInfo)
+      .then(function (scores) {
+        this.setState({
+          scores: scores,
+          isLoading: false
+        })
+      }.bind(this))
+  },
   render: function() {
     return (
-      <Results />
+      <Results
+        isLoading={this.state.isLoading}
+        playersInfo={this.props.location.state.playersInfo}
+        scores={this.state.scores} />
     );
   }
 
