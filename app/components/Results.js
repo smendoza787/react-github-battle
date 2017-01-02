e7a9b764e23677acb19392774e8dcf09a1a5c665
Sergio Mendoza <smendoza787@gmail.com>
2017-01-01 21:14:25 -0800

    added winning and losing score

--- app/components/Results.js
+++ app/components/Results.js
@@ -5,15 +5,17 @@ var UserDetails = require('./UserDetails');
 var UserDetailsWrapper = require('./UserDetailsWrapper');
 
 function Results (props) {
+  var winningIndex = props.scores[0] > props.scores[1] ? 0 : 1;
+  var losingIndex = winningIndex === 0 ? 1 : 0;
   return (
     <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
       <h1>Results</h1>
       <div className="col-sm-8 col-sm-offset-2">
         <UserDetailsWrapper header="Winner">
-          <UserDetails score={props.scores[XX]} info={props.playersInfo[XX]} />
+          <UserDetails score={props.scores[winningIndex]} info={props.playersInfo[winningIndex]} />
         </UserDetailsWrapper>
         <UserDetailsWrapper header="Loser">
-          <UserDetails score={props.scores[XX]} info={props.playersInfo[XX]} />
+          <UserDetails score={props.scores[losingIndex]} info={props.playersInfo[losingIndex]} />
         </UserDetailsWrapper>
       </div>
     </div>
