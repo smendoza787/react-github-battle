928f43e8ff079f96d27152f09568a9e218f27295
Sergio Mendoza <smendoza787@gmail.com>
2017-01-02 20:02:26 -0800

    added Main Container and reusable Loading component

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,14 +1,16 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
 var styles = require('../styles');
-var Link = require('react-router').Link;
-var UserDetails = require('./UserDetails');
+var ReactRouter = require('react-router');
+var Link = ReactRouter.Link;
 var UserDetailsWrapper = require('./UserDetailsWrapper');
+var UserDetails = require('./UserDetails');
 var MainContainer = require('./MainContainer');
+var Loading = require('./Loading');
 
 function ConfirmBattle (props) {
   return props.isLoading === true
-    ? <p> LOADING! </p>
+    ? <Loading />
     : <MainContainer>
         <h1>Confirm Players</h1>
         <div className='col-sm-8 col-sm-offset-2'>
